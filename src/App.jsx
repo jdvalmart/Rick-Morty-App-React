2cbde9df3f2d8ccc0ccc6e1053fe162aca280834
jdvalmart <juanvalencia9411@outlook.com>
2025-12-16 03:02:01 -0500

    proyecto de practica de react, consumo de API, uso de tailwind y manejo de estados

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+
+
+
+function App() {
+  
+  const [characters, setCharacters] = useState([])
+
+  return (
+    <>
+      <div className=''>
+        <h1>Rick and Morty</h1>
+
+      </div>
+    
+    </>
+  )
+}
+
+export default App
